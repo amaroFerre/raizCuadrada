fb61ca559357d099270df8e28b961dd2e4382223
Amarico <Amarico@EquipoAcer>
2018-08-20 11:43:20 +0200

    Importo el paquet java.util.* para poder utilizar el objeto Scanner

---
+++ src/raizCuadrada/RaizCuadrada.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package raizCuadrada;
+
+// Importo el paquete java.util.* para poder utilizar la clase Scanner
+import java.util.*;
+
+/**
+ *
+ * @author Amarico
+ */
+public class RaizCuadrada {
+    
+    
+    
+}
